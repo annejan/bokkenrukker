a6d6c4f1c41c28f8057f8742d7a66d0133d6b02e
Anne Jan Brouwer <brouwer@annejan.com>
2026-06-08 23:19:24 +0200

    chore: stop tracking the build/ directory
    A full CMake build tree (264 files: CMakeCache, object files, the sid2sng
    binary, autogen/moc output) was committed under build/. .gitignore only
    ignored qt/build/, not the top-level build/ used by the root CMakeLists
    wrapper. Add build/ to .gitignore and drop the tree from version control.

--- build/qt/CMakeFiles/goattrk2-qt.dir/__/src/residfp/version.cc.o.d
+++
@@ -1,4 +0,0 @@
-qt/CMakeFiles/goattrk2-qt.dir/__/src/residfp/version.cc.o: \
- /home/paul/work/c64/goattracker2/src/residfp/version.cc \
- /usr/include/stdc-predef.h \
- /home/paul/work/c64/goattracker2/src/residfp/siddefs-fp.h
--- build/qt/CMakeFiles/sid2sng.dir/sid2sng_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-qt/CMakeFiles/sid2sng.dir/sid2sng_autogen/mocs_compilation.cpp.o: \
- /home/paul/work/c64/goattracker2/build/qt/sid2sng_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
